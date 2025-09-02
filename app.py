9dcfebf9d01f341e50c202ba9a57c0fac44414bb
Conrado Guerra <conradoguerra@gmail.com>
2025-09-02 20:10:23 +0200

    Improve code to extract dynamically

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from typing import List
-from flask import Flask
+from flask import Flask, json
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,40 +10,41 @@ def home():
     from infra.client import get
     from service.extract_response import extract_response
 
-    texto = []
-    response = get(
-        "https://www.in.gov.br/consulta/-/buscar/dou?q=%22Tipo+de+Produto%3A+Alimento%22&s=do1&exactDate=all&sortType=0"
+    search_response = get(
+        "https://www.in.gov.br/consulta/-/buscar/dou?q=%22Tipo+de+Produto%3A+Alimento%22&s=do1&exactDate=dia&sortType=0"
     )
-    pattern = r"urlTitle\"\:\"([a-z-0-9.*]+)"
-    found = extract_response(pattern, response.text)
-    response = get(f"https://www.in.gov.br/web/dou/-/{found[0]}")
-    pattern = r"(Empresa: [A-Z\s]+ - CNPJ: [0-9]+)"
-    found = extract_response(pattern, response.text)
-    print(found)
-    texto.append(found[0])
-    pattern = r"(Produto - \(Lote\):[íáçãéóàâêA-Za-z0-9\,\s\(\)\-\º\/\.\;\:\°]+)"
-    found = extract_response(pattern, response.text)
-    print(found)
-    texto.append(found[0])
-    pattern = r"(Ações de fiscalização: [íáçãéA-Za-z0-9]+)"
-    found = extract_response(pattern, response.text)
-    print(found)
-    texto.append(found[0])
-    pattern = r"(Suspensão - [íáçãéA-Za-z0-9\,\s]+)"
-    found = extract_response(pattern, response.text)
-    print(found)
-    texto.append(found[0])
-    pattern = r"(Motivação: [íáçãéóàâêA-Za-z0-9\,\s\(\)\-\º\/\.\;\:\°]+)"
-    found = extract_response(pattern, response.text)
-    print(found)
-    texto.append(found[0])
-    write_in_file(texto)
+    pattern = r"({\"jsonArray\"\s*:\[.*\]})"
+    found = extract_response(pattern, search_response.text)
+    json_response = json.loads(found[0])
+    for resolution in json_response["jsonArray"]:
+        extracted_paragraphs = []
+        url_title = resolution["urlTitle"]
+        resolution_found = get(f"https://www.in.gov.br/web/dou/-/{url_title}")
+        pattern = r"(Empresa: .+)</p>"
+        found = extract_response(pattern, resolution_found.text)
+        extracted_paragraphs.append(found[0])
+        pattern = r"(Produto .+)</p>"
+        found = extract_response(pattern, resolution_found.text)
+        extracted_paragraphs.append(found[0])
+        pattern = r"(Ações de fiscalização: .+)</p>"
+        found = extract_response(pattern, resolution_found.text)
+        extracted_paragraphs.append(found[0])
+        pattern = r"(Suspensão .*|Proibição .*)</p>"
+        found = extract_response(pattern, resolution_found.text)
+        extracted_paragraphs.append(found[0])
+        pattern = r"(Motivação: .+)</p>"
+        found = extract_response(pattern, resolution_found.text)
+        extracted_paragraphs.append(found[0])
+        write_in_file(extracted_paragraphs)
 
-    return response.text
+    if len(json_response["jsonArray"]) > 0:
+        return f"Generated {len(json_response['jsonArray'])} reports"
+    return "No reports generated"
 
 
 def write_in_file(txt: List[str]):
-    with open("test.txt", "w") as file:
+    filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    with open(f"relatorio_{filename}.txt", "w") as file:
         for line in txt:
             file.write(line + "\n")
 
