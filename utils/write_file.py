25cb06634e240154517320d8f80cd00b434f9d0c
Conrado Guerra <conradoguerra@gmail.com>
2025-09-16 20:37:05 +0200

    Add executable and change path to respect executable place

--- utils/write_file.py
+++ utils/write_file.py
@@ -1,10 +1,19 @@
+import os
+import sys
 from datetime import datetime
 from typing import List
 
 
 def write_in_file(txt: List[str]):
     filename = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    print(f"Generated relatorio_{filename}.txt")
-    with open(f"relatorio_{filename}.txt", "w") as file:
+    if getattr(sys, "frozen", False):
+        app_dir = os.path.dirname(sys.executable)
+    else:
+        app_dir = os.path.dirname(os.path.abspath(__file__))
+
+    report_path = os.path.join(app_dir, f"relatorio_{filename}.txt")
+
+    print(f"Generated {report_path}")
+    with open(report_path, "w") as file:
         for line in txt:
             file.write(line + "\n")
